218bfac9088caca9d3e882715a95c5845e91c6b9
C. M. Lin <bagilu@gmail.com>
2026-09-21 15:42:32 +0800

    Add files via upload

---
+++ config-sample.js
@@ -0,0 +1,5 @@
+/* Copy to config.js. Use only a publishable key or legacy anon key. NEVER use service_role / secret keys. */
+window.P127_CONFIG = {
+  SUPABASE_URL: 'https://YOUR_PROJECT.supabase.co',
+  SUPABASE_ANON_KEY: 'YOUR_PUBLISHABLE_OR_ANON_KEY'
+};
---
+++ config.js
@@ -0,0 +1,5 @@
+/* Copy to config.js. Use only a publishable key or legacy anon key. NEVER use service_role / secret keys. */
+window.P127_CONFIG = {
+  SUPABASE_URL: 'https://mfljkyvdadxlrbxlboce.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs'
+};
